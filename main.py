a4d9ba5884f721c759291c65aca6a9f85530c869
NotKonishi <74392505+NotKonishi@users.noreply.github.com>
2022-02-03 11:55:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,26 +13,26 @@ except json.JSONDecodeError:
     login={"email": email, "password": password}
     p.write(str(login))
 
+
 voic=input("Put channel id: ")
-url2=f'https://discord.com/api/v8/channels/{voic}/invites'
+url2=f'https://discord.com/api/v8/channels/{voic}/invites' #create invite discord API v8
 
 aaa=None
 
 def getyt(aaa):
     print(aaa)
-    data = {'max_age': 20, 'max_uses': 3, 'target_application_id': '880218394199220334', 'target_type': 2, 'temporary': False, 'validate': None}
+    data = {'max_age': 20, 'max_uses': 3, 'target_application_id': '880218394199220334', 'target_type': 2, 'temporary': False, 'validate': None} #max_uses=max number of times this invite can be used, target_application_id=the embedded application to open for this voice channel embedded application invite check out https://github.com/NotKonishi/YoutubeTogather-Discord-API/blob/main/activity.txt for more infomation, target_type=the type of target for this voice channel invite
     ab=requests.post(url2, json=data, headers={'Authorization': f'{aaa}'})
     ca=json.loads(ab.text)
-    print('https://discord.gg/' + ca['code'])
+    print('https://discord.gg/' + ca['code']) #result out here
 
-
-url="https://discord.com/api/v9/auth/login"
+url="https://discord.com/api/v9/auth/login" #login account discord API v9
 a=requests.post(url, json={'login': email, 'password': password, 'undelete': False, 'captcha_key': None})
 data2 = json.loads(a.text)
 print(a.text)
 aaa=data2['token']
 if aaa is None:
-    url3='https://discord.com/api/v9/auth/mfa/totp'
+    url3='https://discord.com/api/v9/auth/mfa/totp' #sending code 2FA discord API v9 
     code=input("mfa code: ")
     try:
         pa=requests.post(url3, json={'code': code, 'gift_code_sku_id': None, 'login_source': None, 'ticket': data2['ticket']})
